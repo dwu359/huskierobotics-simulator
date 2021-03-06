55d9dcc1b884325864bd1003b0334262dd72c019
daniel-n6 <54150946+daniel-n6@users.noreply.github.com>
2021-03-05 22:29:17 -0600

    initial commit

---
+++ Constants.java
@@ -0,0 +1,5 @@
+public final class Constants {
+    public static final int[] WINDOW_SIZE = {700, 700}; // in pixels
+    public static final String TITLE = "Swerve Simulator";
+    public static final int DELAY = 40;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is some code that I wrote for the swerve drive robot using Java. The main part of the code is located at InfiniteRecharge2021\bin\main\frc\robot.
